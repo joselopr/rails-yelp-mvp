21f5ff545dce7de609283c12da232641096c259e
jose prieto <josel0priet0@gmail.com>
2021-05-26 14:24:37 +0200

    models created

---
+++ app/models/review.rb
@@ -0,0 +1,8 @@
+class Review < ApplicationRecord
+  belongs_to :restaurant
+
+  RATING = [*0..5]
+
+  validates :content, presence: true
+  validates :rating, presence: true, inclusion: { in: RATING }, numericality: { only_integer: true }
+end
---
+++ db/migrate/20210526111238_update_category_in_restaurant.rb
@@ -0,0 +1,5 @@
+class UpdateCategoryInRestaurant < ActiveRecord::Migration[6.0]
+  def change
+      change_column :restaurants, :category, :string
+  end
+end
